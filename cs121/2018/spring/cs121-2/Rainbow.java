620fc6d399cc919b12eb504fa472e245f9312e37
Marissa Schmidt <marissaschmidt@boisestate.edu>
2018-02-20 11:40:15 -0700

    Adding in-class example from Day 12: ArrayLists (Changed name from BlankCanvas.java to Rainbow.java

--- cs121/2018/spring/cs121-2/Rainbow.java
+++ cs121/2018/spring/cs121-2/Rainbow.java
@@ -1,49 +1,108 @@
-import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
+import java.awt.Color;
 
-public class Rainbow
+/**
+ * A starting point for creating a new graphical program
+ * @author CS121 instructors
+ */
+@SuppressWarnings("serial")
+public class Rainbow extends JPanel
 {
-	public static void main(String[] args)
+	/**
+	 * Draws the picture in the panel. This is where all of your
+	 * code will go.
+	 * @param page Our graphics canvas.
+	 */
+	public void paintComponent (Graphics page)
 	{
-		Random rand = new Random();
-		ArrayList<Color> rainbow = new ArrayList<Color>();
+		int width = getWidth();
+		int height = getHeight();
 		
+		Random random = new Random();
+		/* Start with BlankCanvas.java from graphics examples */
+		/* Instantiate an empty list of colors */
+		ArrayList<Color> colors = new ArrayList<Color>();
+		
+		/* Use a for loop to add n random colors */
 		int n = 10;
 		for(int i = 0; i < n; i++)
 		{
-			Color c = new Color(rand.nextInt(256), rand.nextInt(256),rand.nextInt(256));
-			rainbow.add(c);
+			int r = random.nextInt(256);
+			int g = random.nextInt(256);
+			int b = random.nextInt(256);
+			colors.add(new Color(r, g, b));
 		}
 		
-		rainbow.remove(2);
-		
-		Color orange = rainbow.get(1);
-		rainbow.remove(orange);
-		
-		System.out.println("My rainbow: " + rainbow);
-		
-		System.out.println("There are " + rainbow.size() + " elements");
-		
-		if(rainbow.contains(Color.YELLOW))
+		/* Print each color using for loop (e.g. Color i: java.awt.Color)*/
+		for(int i = 0; i < colors.size(); i++)
 		{
-			System.out.println("It has yellow!");
+			Color c = colors.get(i);
+			System.out.println("Color " + (i + 1) + ": " + c);
 		}
-		else
+		
+		/* Print each color using for-each loop (no index) */
+		int count = 0;
+		for(Color c : colors)
 		{
-			System.out.println("It does not have yellow!");
+			int green = c.getGreen();
+			System.out.println("Color " + (count + 1) + ": " + c);
+			count++;
 		}
 		
-		System.out.println("My rainbow: " + rainbow);
-		
-		for(int i = 0; i < rainbow.size(); i++)
+		/* Use for loop to draw dot (use i for x offset) */
+		/* Also need to calculate circleWidth */
+		int circleWidth = width / n;
+		for(int i = 0; i < colors.size(); i++)
 		{
-			Color color = rainbow.get(i);
-			System.out.println(color);
+			Color c = colors.get(i);
+			page.setColor(c);
+			page.fillOval(i*circleWidth, 0, circleWidth, circleWidth);
 		}
 		
-		System.out.println("There are " + rainbow.size() + " elements");
-
+		/* Use for-each loop to find color with max green value */
+		
+		Color maxGreenColor = Color.BLACK;
+		for(Color c : colors)
+		{
+			if(c.getGreen() > maxGreenColor.getGreen())
+			{
+				maxGreenColor = c;
+			}
+		}
+		System.out.println("Max Green Color " + maxGreenColor);
+		
+		/* Draw large oval with the max green color */
+		page.setColor(maxGreenColor);
+		page.fillOval(0, circleWidth, circleWidth*3, circleWidth*3);
 	}
 
+
+	/**
+	 * Constructor (panel initialization)
+	 */
+	public Rainbow()
+	{
+		this.setBackground(Color.white);
+		this.setPreferredSize(new Dimension(800, 600));
+	}
+
+
+	/**
+	 * Sets up a JFrame and the BlankCanvas panel.
+	 * @param args unused
+	 */
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("BlankCanvas");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new BlankCanvas());
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
