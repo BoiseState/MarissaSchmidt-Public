2b1ef73ce335dec3c0ea1ebbf714bded183a1475
Marissa Schmidt <marissa@localhost.localdomain>
2019-01-29 11:21:25 -0700

    Adding Lesson 4 in-class example

--- cs121/2019/spring/cs121-1/MyObjects.java
+++ cs121/2019/spring/cs121-1/MyObjects.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.awt.Point;
+import java.awt.Color;
 /**
  * This example demonstrates how to create and use various objects.
  * 
@@ -8,13 +11,32 @@ public class MyObjects
 	public static void main(String[] args)
 	{
 		// Instantiate a String object
+		String name = new String("Snoopy");
+		String otherName = "Linus";
 		
-		// Instantiate a Scanner object (which methods do we know?)
+		// Instantiate a Scanner object using constructor
+		Scanner scan = new Scanner(System.in);
+		// Invoke nextInt() and close() methods using dot operator
+		System.out.print("Enter x: ");
+		int x = scan.nextInt();
+		scan.close();
 		
-		// Instantiate a Point object (show documentation, get, set)
+		// Instantiate a Point object using constructor
+		Point coordinate = new Point(x, 200);
+		System.out.println("My point is: " + coordinate);
 		
-		// Instantiate a Color object (show documentation)
+		// Invoke setLocation(x, y) method using dot operator
+		coordinate.setLocation(34, 45);
+		System.out.println("My point is: " + coordinate);
 		
-		// Instantiate a MiniFig object (show documentation, example)
+		// Invoke getLocation() method using dot operator and store
+		// returned result in a variable
+		Point location = coordinate.getLocation();
+		int yOffset = location.y;
+		System.out.println("Y: " + yOffset);
+		
+		// Instantiate a Color object using constructor
+		Color myColor = new Color(128, 0, 0);
+		int blue = myColor.getBlue();
 	}
 }
