ea90000fff0211ecc749f67a5c7265881793544c
Marissa Schmidt <marissaschmidt@boisestate.edu>
2020-01-14 14:00:55 -0700

    Lesson 0: Importing examples

---
+++ cs121/2020/spring/cs121-2/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * This is a program that prints Hello World!
+ *
+ * Marissa Schmidt
+ * CS121-002, Spring 2020
+ */
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
