c7c56b2d3ce8232068eedbfeb5df35692094fbf8
Marissa Schmidt <marissaschmidt@boisestate.edu>
2020-01-24 14:37:36 -0700

    Resolved conflicts

--- cs121/2019/fall/cs121-6/DogManagerPanel.java
+++ cs121/2019/fall/cs121-6/DogManagerPanel.java
@@ -27,18 +27,40 @@ public class DogManagerPanel extends JPanel
 	 */
 	public DogManagerPanel()
 	{
-		Dog dog = new Dog("Norman", 123);
-		dog.setBreed(Dog.Breed.BEAGLE);
-		
+		this.setPreferredSize(new Dimension(500, 400));	
+
 		// Step 1: Instantiate components
 		title = new JLabel("Welcome to Ultimate Dog Manager");
-		dogButton = new DogButton(dog);
 		
 		// Step 2: Add components to this panel
 		add(title);
-		add(dogButton);
 		
-		this.setPreferredSize(new Dimension(300, 400));		
+		// Create a new Dog object.
+		Dog rover = new Dog("Rover", 1234);
+		rover.setBreed(Dog.Breed.GOLDEN_RETRIEVER);
+		rover.setImagePath("1234-rover.jpg");
+		
+		// Create and add a button for the dog to this panel.
+		DogButton dogButton1 = new DogButton(rover);
+		this.add(dogButton1);
+		
+		// Create a second Dog object.
+		Dog cloud = new Dog("Cloud", 1235);
+		cloud.setBreed(Dog.Breed.POMERANIAN);
+		cloud.setImagePath("1235-cloud.jpg");
+		
+		// Create and add a button for the dog to this panel.
+		DogButton dogButton2 = new DogButton(cloud);
+		this.add(dogButton2);
+		
+		// Create a third Dog object.
+		Dog beethoven = new Dog("Beethoven", 1235);
+		beethoven.setBreed(Dog.Breed.ST_BERNARD);
+		beethoven.setImagePath("1236-beethoven.jpg");
+		
+		// Create and add a button for the dog to this panel.
+		DogButton dogButton3 = new DogButton(beethoven);
+		this.add(dogButton3);
 	}
 }
 
