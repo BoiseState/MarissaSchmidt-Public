c8e87ccf61e12c34ce9fb9d49d64c0d1c89f8b6d
Marissa Schmidt <marissaschmidt@boisestate.edu>
2020-03-16 08:40:10 -0600

    Adding Menu with Switch Statement example

---
+++ cs121/2020/spring/Lesson09/MenuOption.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+/**
+ * Demonstrates how to use a switch-statement and a while loop with a sentinel
+ * value for a basic interactive menu.
+ * 
+ * @author CS121 Instructors
+ */
+public class MenuOption
+{
+	public static void main(String[] args)
+	{
+		final String QUIT = "q";
+		
+		Scanner kbd = new Scanner(System.in);
+		
+		String input = ""; // starting condition
+
+		System.out.println("==========================");
+		System.out.println("(j) Tell a joke");
+		System.out.println("(a) Print art");
+		System.out.println("(e) Equation");
+		System.out.println("(m) Menu");
+		System.out.println("(q) Quit") ;
+		System.out.println("==========================");
+
+
+		while(!input.equals(QUIT)) // ending condition
+		{
+			System.out.print("What do you want me to do? ");
+			input = kbd.nextLine().toLowerCase();
+			
+			switch(input)
+			{
+			case "j":
+				System.out.println("Time flies like an arrow; fruit flies like a banana.");
+				break;
+			case "a":
+				System.out.println("**~~~*.oOo.*~~~**");
+				break;
+			case "e":
+				System.out.println("e = mc\u00B2");
+				break;
+			case "m":
+				System.out.println("==========================");
+				System.out.println("(j) Tell a joke");
+				System.out.println("(a) Print art");
+				System.out.println("(e) Equation");
+				System.out.println("(m) Menu");
+				System.out.println("(q) Quit") ;
+				System.out.println("==========================");
+				break;
+			case "q":
+				System.out.println("Goodbye.");
+				break;
+			default:
+				System.out.println("Invalid option");
+				break;
+			}
+		}
+		System.out.println("Exiting.");
+		
+		kbd.close();
+	}
+
+}
